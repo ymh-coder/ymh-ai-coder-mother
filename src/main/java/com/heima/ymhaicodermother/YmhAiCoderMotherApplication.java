73550ae00a57b6dd570c8299454059b7ba066e18
melody <1097379599@qq.com>
2026-04-01 19:18:10 +0800

    初始化前端页面和后端用户模块

--- src/main/java/com/heima/ymhaicodermother/YmhAiCoderMotherApplication.java
+++ src/main/java/com/heima/ymhaicodermother/YmhAiCoderMotherApplication.java
@@ -1,9 +1,11 @@
 package com.heima.ymhaicodermother;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.heima.ymhaicodermother.mapper")
 public class YmhAiCoderMotherApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/heima/ymhaicodermother/generator/MyBatisCodeGenerator.java
@@ -0,0 +1,84 @@
+package com.heima.ymhaicodermother.generator;
+
+import cn.hutool.core.lang.Dict;
+import cn.hutool.setting.yaml.YamlUtil;
+import com.mybatisflex.codegen.Generator;
+import com.mybatisflex.codegen.config.GlobalConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.util.Map;
+
+/**
+ * MyBatis Code Generator 类
+ * 用于根据数据库表结构自动生成 MyBatis-Flex 相关代码
+ */
+public class MyBatisCodeGenerator {
+
+    // 需要生成的表名数组
+    private static final String[] TABLE_NAMES = {"user"};
+
+    /**
+     * 程序入口方法
+     * @param args 命令行参数
+     */
+    public static void main(String[] args) {
+        // 获取数据源信息
+        Dict dict = YamlUtil.loadByPath("application.yml");
+        Map<String, Object> dataSourceConfig = dict.getByPath("spring.datasource");
+        String url = String.valueOf(dataSourceConfig.get("url"));
+        String username = String.valueOf(dataSourceConfig.get("username"));
+        String password = String.valueOf(dataSourceConfig.get("password"));
+        // 配置数据源
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        // 创建配置内容
+        GlobalConfig globalConfig = createGlobalConfig();
+
+        // 通过 datasource 和 globalConfig 创建代码生成器
+        Generator generator = new Generator(dataSource, globalConfig);
+
+        // 生成代码
+        generator.generate();
+    }
+
+    // 详细配置见：https://mybatis-flex.com/zh/others/codegen.html
+    public static GlobalConfig createGlobalConfig() {
+        // 创建配置内容
+        GlobalConfig globalConfig = new GlobalConfig();
+
+        // 设置根包，建议先生成到一个临时目录下，生成代码后，再移动到项目目录下
+        globalConfig.getPackageConfig()
+                .setBasePackage("com.heima.ymhaicodemother.genresult");
+
+        // 设置表前缀和只生成哪些表，setGenerateTable 未配置时，生成所有表
+        globalConfig.getStrategyConfig()
+                .setGenerateTable(TABLE_NAMES)
+                // 设置逻辑删除的默认字段名称
+                .setLogicDeleteColumn("isDelete");
+
+        // 设置生成 entity 并启用 Lombok
+        globalConfig.enableEntity()
+                .setWithLombok(true)
+                .setJdkVersion(21);
+
+        // 设置生成 mapper
+        globalConfig.enableMapper();
+        globalConfig.enableMapperXml();
+
+        // 设置生成 service
+        globalConfig.enableService();
+        globalConfig.enableServiceImpl();
+
+        // 设置生成 controller
+        globalConfig.enableController();
+
+        // 设置生成时间和字符串为空，避免多余的代码改动
+        globalConfig.getJavadocConfig()
+                .setAuthor("<a href=\"https://github.com/ymh-coder\">程序员ymh-coder</a>")
+                .setSince("");
+        return globalConfig;
+    }
+}
