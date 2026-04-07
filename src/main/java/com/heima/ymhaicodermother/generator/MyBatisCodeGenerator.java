bd28cf279990aa6c5135cc6b55530b8c204557ea
melody <1097379599@qq.com>
2026-04-07 16:54:17 +0800

    新增应用模块模型和数据库表

--- src/main/java/com/heima/ymhaicodermother/generator/MyBatisCodeGenerator.java
+++ src/main/java/com/heima/ymhaicodermother/generator/MyBatisCodeGenerator.java
@@ -15,7 +15,7 @@ import java.util.Map;
 public class MyBatisCodeGenerator {
 
     // 需要生成的表名数组
-    private static final String[] TABLE_NAMES = {"user"};
+    private static final String[] TABLE_NAMES = {"app"};
 
     /**
      * 程序入口方法
